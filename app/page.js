2d55fec8ae316eac9c4fc027a29d57f350d11061
Samarth Bhatia <samarthbhatia77@gmail.com>
2025-06-30 22:14:42 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>Welcome to the todo app, chose the "ADD" option above to add todos!</p>
+    <p className="font-mono text-2xl">Welcome to the todo app, chose the "ADD" option above to add todos!</p>
   );
 }
